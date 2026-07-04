70746fd9a7ff6cfd852a71a1e69de68ea8f2f16b
Nada Tarek <nadaelkomey277@gmail.com>
2026-07-04 09:39:46 +0300

    Add task1 script.js

---
+++ task1/script.js
@@ -0,0 +1,9 @@
+var r = Math.floor(Math.random() * 256);
+var g = Math.floor(Math.random() * 256);
+var b = Math.floor(Math.random() * 256);
+
+document.write(`
+<h1 style="color: rgb(${r}, ${g}, ${b})">
+Welcome to JavaScript!
+</h1>
+`);
